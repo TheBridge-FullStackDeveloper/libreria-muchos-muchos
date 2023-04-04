87b3ca592f2209060dcac820209ba08acc55cc0d
SofiaPinilla <contact@sofiapinilla.com>
2023-04-04 10:51:04 +0200

    correo de confirmación añadido

---
+++ controllers/UserController.js
@@ -0,0 +1,85 @@
+const { User } = require("../models/index");
+const transporter = require("../config/nodemailer");
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const {jwt_secret} = require("../config/config.json")["development"]
+const UserController = {
+  async create(req, res, next) {
+    try {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+      const user = await User.create({
+        ...req.body,
+        password: hash,
+        confirmed: false,
+        role: "user",
+      });
+      const emailToken = jwt.sign({ email: req.body.email }, jwt_secret, {
+        expiresIn: "48h",
+      });
+      const url = "http://localhost:3000/users/confirm/" + emailToken;
+      await transporter.sendMail({
+        to: req.body.email,
+        subject: "Confirme su registro",
+        html: `<h3>Bienvenido, estas a un paso de registrarte </h3>
+            <a href="${url}"> Click para confirmar tu registro</a>
+            Este enlace caduca en 48h.
+            `,
+      });
+
+      res.status(201).send({
+        message: "Te hemos enviado un correo para confirmar el registro",
+        user,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  },
+
+  async login(req, res) {
+    try {
+      const user = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+      if (!user) {
+        return res
+          .status(400)
+          .send({ message: "Usuario o contraseña incorrectos" });
+      }
+      if (!user.confirmed) {
+        return res.status(400).send({ message: "Debes confirmar tu correo" });
+      }
+      const isMatch = bcrypt.compareSync(req.body.password, user.password); //comparo contraseñas
+      if (!isMatch) {
+        return res
+          .status(400)
+          .send({ message: "Usuario o contraseña incorrectos" });
+      }
+      res.send({ message: "Bienvenid@ " + user.name, user });
+    } catch (err) {
+      console.error(err);
+    }
+  },
+
+  async confirm(req, res) {
+    try {
+      const token = req.params.emailToken;
+      const payload = jwt.verify(token, jwt_secret);
+      User.update(
+        { confirmed: true },
+        {
+          where: {
+            email: payload.email,
+          },
+        }
+      );
+      res.status(201).send("Usuario confirmado con éxito");
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
+
+module.exports = UserController
--- index.js
+++ index.js
@@ -6,5 +6,6 @@ app.use(express.json()); //parseamos el body
 
 app.use("/genres", require("./routes/genres"));
 app.use("/books", require("./routes/books"));
+app.use("/users", require("./routes/users"));
 
 app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}`));
