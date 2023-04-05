3d4c01e9edbde117b052343ce86f0874056b167f
SofiaPinilla <contact@sofiapinilla.com>
2023-04-05 12:32:38 +0200

    error asincronia test arreglado

--- tests/users.test.js
+++ tests/users.test.js
@@ -4,11 +4,8 @@ const { User } = require("../models/index.js");
 const jwt = require("jsonwebtoken");
 const { jwt_secret } = require("../config/config.json")["development"];
 describe("testing/users", () => {
-  afterAll(() => {
-    return User.destroy({ where: {}, truncate: true });
-  });
-
   let token;
+  let resUser;
   const user = {
     name: "Username",
     email: "test@example.com",
@@ -17,66 +14,71 @@ describe("testing/users", () => {
     confirmed: false,
   };
 
-  test("Create a user", async () => {
-    let usersCount = await User.count();
-    expect(usersCount).toBe(0);
-
-    const res = await request(app)
-      .post("/users/register")
-      .send(user)
-      .expect(201);
-    const sendUser = {
-      ...user,
-      id: res.body.user.id,
-      password: res.body.user.password,
-      createdAt: res.body.user.createdAt,
-      updatedAt: res.body.user.updatedAt,
-    };
-    const newUser = res.body.user;
-    usersCount = await User.count();
-    expect(usersCount).toBe(1);
-    expect(res.body.user.id).toBeDefined();
-    expect(res.body.user.createdAt).toBeDefined();
-    expect(res.body.user.updatedAt).toBeDefined();
-    expect(newUser).toEqual(sendUser);
-  });
-  test("Confirm a user", async () => {
+  beforeEach(async () => {
+    // Create a user and log in before each test
+    resUser = await request(app).post("/users/register").send(user).expect(201);
     const emailToken = jwt.sign({ email: user.email }, jwt_secret, {
       expiresIn: "48h",
     });
-    const res = await request(app)
+    await request(app)
       .get("/users/confirm/" + emailToken)
       .expect(201);
-    expect(res.text).toBe("Usuario confirmado con éxito");
+    const res = await request(app)
+      .post("/users/login")
+      .send({ email: "test@example.com", password: "123456" })
+      .expect(200);
+    token = res.body.token;
   });
+
+  afterAll(() => {
+    return User.destroy({ where: {}, truncate: true });
+  });
+
+  test("Create a user", async () => {
+    let usersCount = await User.count();
+    expect(usersCount).toBe(1);
+    const sendUser = {
+      ...user,
+      id: resUser.body.user.id,
+      password: resUser.body.user.password,
+      createdAt: resUser.body.user.createdAt,
+      updatedAt: resUser.body.user.updatedAt,
+    };
+    const newUser = resUser.body.user;
+    usersCount = await User.count();
+    expect(usersCount).toBe(1);
+    expect(resUser.body.user.id).toBeDefined();
+    expect(resUser.body.user.createdAt).toBeDefined();
+    expect(resUser.body.user.updatedAt).toBeDefined();
+    expect(newUser).toEqual(sendUser);
+  });
+
   test("Login a user", async () => {
     const res = await request(app)
       .post("/users/login")
       .send({ email: "test@example.com", password: "123456" })
       .expect(200);
-    console.warn(res.body.token);
     token = res.body.token;
   });
+
   test("Get users", async () => {
-    setTimeout(async () => {
-      const res = await request(app)
-        .get("/users")
-        .expect(200)
-        .set({ Authorization: token });
-      expect(res.body).toBeInstanceOf(Array);
-    }, 2000);
+    const res = await request(app)
+      .get("/users")
+      .expect(200)
+      .set({ Authorization: token });
+    expect(res.body).toBeInstanceOf(Array);
   });
+
   test("Update a user record", async () => {
     const updateUser = { name: "Updated name" };
-    setTimeout(async () => {
-      const res = await request(app)
-        .put("/users/id/1")
-        .send(updateUser)
-        .set({ Authorization: token })
-        .expect(200);
-      expect(res.text).toBe("Usuario actualizado con éxito");
-    });
-  }, 2000);
+    const res = await request(app)
+      .put("/users/id/1")
+      .send(updateUser)
+      .set({ Authorization: token })
+      .expect(200);
+    expect(res.text).toBe("Usuario actualizado con éxito");
+  });
+
   test("Logout a user record", async () => {
     const res = await request(app)
       .delete("/users/logout")
